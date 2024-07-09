d14f7d3e89f275ae98cb8d7f26df2faa309a09e7
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2024-07-09 14:21:01 +0700

    Pengenalan Lambda

---
+++ src/programmer/zaman/now/lambda/app/SimpleActionApp.java
@@ -0,0 +1,23 @@
+package programmer.zaman.now.lambda.app;
+
+import programmer.zaman.now.lambda.SimpleAction;
+
+public class SimpleActionApp {
+    public static void main(String[] args) {
+
+        SimpleAction simpleAction1 = new SimpleAction() {
+            @Override
+            public String action() {
+                return "Eko";
+            }
+        };
+
+        System.out.println(simpleAction1.action());
+
+        SimpleAction simpleAction2 = () -> {
+            return "Eko";
+        };
+
+        System.out.println(simpleAction2.action());
+    }
+}
